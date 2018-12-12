cbd7abf42a8b26db9db69cfa4db1e346231a8583
saurabhmj <saurabhjogalekar@gmail.com>
2018-12-12 15:24:35 -0600

    Added duplication logic

--- query2_join.py
+++ query2_join.py
@@ -2,22 +2,27 @@ from pyspark import SparkConf, SparkContext
 from pyspark.sql import HiveContext
 from pyspark.sql.functions import col
 from pyspark.sql.functions import array
+from pyspark.sql.functions import collect_list
+from pyspark.sql.functions import concat_ws
 
-conf = SparkConf() 
+conf = SparkConf()
 sc = SparkContext(conf = conf)
-hive_context = HiveContext(sc) 
+hive_context = HiveContext(sc)
 
 
-airlines = hive_context.table("flight.flight_data_orc2") 
+airlines = hive_context.table("flight.flight_data_orc2")
 airports = hive_context.table("flight.airport_lookup")
 carriers = hive_context.table("flight.carrier_lookup")
 
-carrier_desc = "carrier_desc" 
-origin_desc = "origin_desc" 
+carrier_desc = "carrier_desc"
+origin_desc = "origin_desc"
 dest_desc = "dest_desc"
 
+#we do not observe any duplicates on airport lookup
 airports = airports.dropDuplicates(['code'])
-carriers = carriers.dropDuplicates(['code'])
+
+#we group the carriers over code and create a list of all possible descriptions. Then, we concat the descriptions as a string with '||' as a separator 
+carriers = carriers.groupBy("code").agg(collect_list(carriers.description).alias('new_desc')).select([col("code"),concat_ws(" || ", col("new_desc")).alias("description")])
 
 air_car = airlines.join(carriers, airlines.carrier == carriers.code).select([a for a in airlines.columns] + [carriers.description.alias(carrier_desc)])
 
@@ -34,7 +39,5 @@ print combined.head(2)
 print "\n\n\n"
 
 combined = combined.where(combined.month == 3).where(combined.day_of_month == 14).groupBy("carrier_desc").sum("arr_delay").withColumnRenamed("sum(arr_delay)", "sum_arr_delay").sort(col("sum_arr_delay").desc()).limit(5)
-
-#combined = combined.groupBy("origin_desc").avg("dep_delay").withColumnRenamed("avg(dep_delay)", "avg_dep_delay").sort(col("avg_dep_delay").desc()).limit(5)
 
 combined.write.format("csv").save("file:///root/flight_data/query22.csv")
